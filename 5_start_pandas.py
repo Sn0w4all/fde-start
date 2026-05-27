1e17490f41dd5294fdf2c194435d30f0d2fd07b5
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-05-27 22:27:48 +0300

    Auto commit: update 5_start_pandas.py and LOG.md

--- 5_start_pandas.py
+++ 5_start_pandas.py
@@ -1,4 +1,46 @@
 import numpy as np
 import pandas as pd
 
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(s)
 
+# указываем начало временнОго периода и число повторений (дни по умолчанию)
+dates = pd.date_range('20130101', periods=6)
+print(dates)
+
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print(df)
+
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'), # временнАя метка
+                    'C': pd.Series(range(4), index=list(range(4)), dtype='float32'), # Серия на основе списка
+                    'D': np.array([3] * 4, dtype='int32'), # массив целых чисел NumPy 
+                    'E': pd.Categorical(["test", "train", "test", "train"]), # категории
+                    'F': 'foo'})
+
+print(df)
+print('')
+#print(df2.head())
+#print('')
+#print(df2.to_numpy()) #массив с одним типом строк - object c разными типами данных
+#print('')
+#print(df.to_numpy()) #массив с одним типом строк - object с int-ом (оптимизация типов данных)
+
+#print(df2.dtypes)
+#df=df.T
+df.sort_values(by='B')
+print(df)
+
+df3 = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print(df3["Age"])
