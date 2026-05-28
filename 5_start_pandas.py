b683d33c4e33a6c1d1826919e97fc6cf1c8bbf7b
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-05-28 10:42:27 +0300

    replase

--- 5_start_pandas.py
+++ 5_start_pandas.py
@@ -5,7 +5,7 @@ import pandas as pd
 portfolio = pd.read_csv(
     "portfolio.csv",
     sep=';',
-    decimal=',',
+#    decimal=',',
     encoding='utf-8-sig'
 )
 
@@ -13,55 +13,49 @@ portfolio = pd.read_csv(
 portfolio.dtypes
 portfolio.info()
 
-commission = portfolio[portfolio["Операция"].isin(['Брокерская комиссия', 'Услуги сторонних организаций'])]
-#portfolio.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
-#commission.head(12)
-#commission["Объем транзакции"].describe()
-abs(commission["Объем транзакции"].sum())
-commission.groupby("Операция")["Объем транзакции"].sum()
+try:
+#portfolio["Объем транзакции"] = portfolio["Объем транзакции"].str.replace('+','')
+    portfolio["Объем транзакции"] = portfolio["Объем транзакции"].str.replace(',','.').astype(float)
 
-# эксперименты с датафреймами и сериями
+    commission = portfolio[portfolio["Операция"].isin(['Брокерская комиссия', 'Услуги сторонних организаций'])]
+    #portfolio.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
+    #commission.head(12)
+    #commission["Объем транзакции"].describe()
+    sumComission = abs(commission["Объем транзакции"].sum())
+    commission.groupby("Операция")["Объем транзакции"].sum()
+    print(sumComission)
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
-print(s)
+    # эксперименты с датафреймами и сериями
 
-# указываем начало временнОго периода и число повторений (дни по умолчанию)
-dates = pd.date_range('20130101', periods=6)
-print(dates)
+    s = pd.Series([1, 3, 5, np.nan, 6, 8])
 
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
-print(df)
 
-df2 = pd.DataFrame({'A': 1.,
-                    'B': pd.Timestamp('20130102'), # временнАя метка
-                    'C': pd.Series(range(4), index=list(range(4)), dtype='float32'), # Серия на основе списка
-                    'D': np.array([3] * 4, dtype='int32'), # массив целых чисел NumPy 
-                    'E': pd.Categorical(["test", "train", "test", "train"]), # категории
-                    'F': 'foo'})
+    # указываем начало временнОго периода и число повторений (дни по умолчанию)
+    dates = pd.date_range('20130101', periods=6)
 
-print(df)
-print('')
-#print(df2.head())
-#print('')
-#print(df2.to_numpy()) #массив с одним типом строк - object c разными типами данных
-#print('')
-#print(df.to_numpy()) #массив с одним типом строк - object с int-ом (оптимизация типов данных)
 
-#print(df2.dtypes)
-#df=df.T
-df.sort_values(by='B')
-print(df)
+    df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 
-df3 = pd.DataFrame(
-    {
-        "Name": [
-            "Braund, Mr. Owen Harris",
-            "Allen, Mr. William Henry",
-            "Bonnell, Miss. Elizabeth",
-        ],
-        "Age": [22, 35, 58],
-        "Sex": ["male", "male", "female"],
-    }
-)
 
-print(df3["Age"])
+    df2 = pd.DataFrame({'A': 1.,
+                        'B': pd.Timestamp('20130102'), # временнАя метка
+                        'C': pd.Series(range(4), index=list(range(4)), dtype='float32'), # Серия на основе списка
+                        'D': np.array([3] * 4, dtype='int32'), # массив целых чисел NumPy 
+                        'E': pd.Categorical(["test", "train", "test", "train"]), # категории
+                        'F': 'foo'})
+
+
+    #print(df2.head())
+    #print('')
+    #print(df2.to_numpy()) #массив с одним типом строк - object c разными типами данных
+    #print('')
+    #print(df.to_numpy()) #массив с одним типом строк - object с int-ом (оптимизация типов данных)
+
+    #print(df2.dtypes)
+    #df=df.T
+    df.sort_values(by='B')
+
+
+
+except ValueError: 
+    print('Битый файл')
