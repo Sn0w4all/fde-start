e44a35680d1f7f4ef68b111c4da5bd8f942d4052
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-05-28 14:53:43 +0300

    fin task with pandas

--- 6_pandas_tasks.py
+++ 6_pandas_tasks.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+#import numpy as np
 #import matplotlib.pyplot as plt
 
 portfolio = pd.read_csv(
@@ -21,16 +21,23 @@ try:
     #commission.head(12)
     #commission["Объем транзакции"].describe()
     
-
     sumComission = abs(commission["Объем транзакции"].sum())
     dateTo = commission["Дата"].max()
     dateFrom = commission["Дата"].min()
     print('Общая комиссия за период с ',dateFrom,' по ',dateTo,' составила ',sumComission, 'RUB')
     #print(commission.iloc[2:17, 2:7]) #строки, столбцы
     
-    commissionByType = commission.groupby("Операция")["Объем транзакции"].sum()
-    print(commissionByType)
-    commissionByType.to_excel("commissionByType.xlsx", sheet_name="passengers", index=True)
+    commissionByType = commission.groupby("Операция")["Объем транзакции"].sum() #это сервиес
+    course = 70.23
+
+    #df2Commission = pd.DataFrame({'Объем транзакции': commissionByType}) #можно так
+    df2Commission = commissionByType.to_frame() #или так
+
+#   commissionByType = commissionByType.T
+#   print(commissionByType)
+    df2Commission["Валюта"] = 'RUB'
+    df2Commission["Сумма в USD"] = round(df2Commission["Объем транзакции"] / course,2)
+    df2Commission.to_excel("commissionByType.xlsx", sheet_name="passengers", index=True)
     
 
 except ValueError: 
