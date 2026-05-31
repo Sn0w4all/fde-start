53aa182b3cf6c16729e5ef84d8a529773ba4da35
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-05-31 16:22:44 +0300

    firstcall LLM

---
+++ 7_hello_llm.py
@@ -0,0 +1,20 @@
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
+load_dotenv("claude_api.env")
+
+client = Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_KEY"),  # This is the default and can be omitted
+)
+
+message = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello, Claude",
+        }
+    ],
+    model="claude-haiku-4-5"
+)
+print(message.content)
