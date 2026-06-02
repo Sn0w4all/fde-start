aa901ae7130dfc6057d8fdfad18438d0f3bd5ab0
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-06-02 17:12:33 +0300

    w2 d7

---
+++ 8_dial_llm.py
@@ -0,0 +1,40 @@
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
+load_dotenv("claude_api.env")
+
+client = Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_KEY"),  # This is the default and can be omitted
+)
+
+messages = []
+
+def chat(text):
+    messages.append({"role": "user", "content":text})
+    
+    message = client.messages.create(
+        max_tokens=1024,
+        messages= messages,
+        model="claude-haiku-4-5"
+    )
+    answer = message.content[0].text
+    if message.content[0].stop_reason == 'end_turn':
+        messages.append({"role": "assistant", "content":answer})
+        return answer   
+    else:
+        return ''
+
+for i in range(0,12,3):
+    print('Твой ввод №', str(i),':')
+    input = input()
+    answer = chat(input)
+    if answer != "":
+        print('Ответ LLM:', answer) 
+        #попробовать 2 ввода ебануть
+        #print('Ответ LLM:', answer) 
+        
+    else:
+        print('Токены закончились')
+        break
+
+print('Сессия закончилась')
