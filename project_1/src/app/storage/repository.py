cbb3cd60cb7fab685e7ee3254cc45bea12825ccf
ZotovAntonCl <mc.zotovanton@gmail.com>
2026-06-15 00:10:43 +0300

    w3

--- project_1/src/app/storage/repository.py
+++ project_1/src/app/storage/repository.py
@@ -10,8 +10,46 @@ def save_data(data, data_type = "raw"):
     core_folder = Path(__file__).parent.parent.parent.parent / "data"
     folder = Path(f"{core_folder}/{data_type}_{date}")
     folder.mkdir(parents=True, exist_ok=True)   # создаст папку, если её нет
-    path = folder / f"data_{time}.json"
+    path = folder / f"{data_type}_data_{time}.json"
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
     logger.info("Saving %s data to %s", data_type, path)
+
+
+def load_latest_data(data_type  = "raw"):
+    #Находит самый свежий JSON файл в папке и подпапках и загружает его
+    
+    folder = Path(__file__).parent.parent.parent.parent / "data"
+    folder_path = str(folder)
+    if not folder.exists():
+        logger.error("Папки нет. Данных в %s пока не существует", folder_path)
+        return None
+
+    
+    # Находим все JSON файлы рекурсивно (в папке и подпапках)
+    json_files = list(folder.rglob("*.json"))
+    
+    # Фильтруем по содержимому имени файла, если задано
+    
+    json_files = [
+        f 
+        for f in json_files 
+        if data_type in f.name
+    ]
+    
+    if not json_files:
+        logger.warning("JSON файлов нет в %s", folder_path)
+        return None
+    
+    # Находим самый свежий файл
+    latest_file = max(json_files, key=lambda p: p.stat().st_mtime)
+    
+    try:
+        with open(latest_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        logger.info("Загружены данные из: %s", latest_file)
+        return data
+    except Exception as e:
+        logger.error("Ошибка при загрузке файла %s: %s", latest_file, e)
+        return None
     
--- project_1/src/app/transformers/normalize.py
+++ project_1/src/app/transformers/normalize.py
@@ -1,17 +1,22 @@
+from ..storage.repository import load_latest_data
+
 import logging
 logger = logging.getLogger(__name__)
 
 def normalize_data(raw_data):
-
-    normalized_data = {}
+    normalized_data = load_latest_data("normalized")
+    if normalized_data is None:
+        normalized_data = {}
+    print(normalized_data)
     for ticker, ticker_data in raw_data.items():
+        normalized_data.setdefault(ticker, [])
+
         columns = ticker_data["marketdata"]["columns"]
         data = ticker_data["marketdata"]["data"]
         if not data:  # Пропускаем пустые данные
-            normalized_data[ticker] = []
             logger.info("No data available for ticker %s", ticker)
             continue
-        normalized_data[ticker] = []
+
         high_index = columns.index("HIGH")  # найти индекс колонки HIGH
         low_index = columns.index("LOW")  # найти индекс колонки LOW
         now_index = columns.index("LAST")  # найти индекс колонки CLOSE
@@ -20,15 +25,17 @@ def normalize_data(raw_data):
         for entry in data:
             if entry[boardid_index] != "CETS":
                 continue
-            normalized_data[ticker].append({
+            new_normalized_data = {
                 "date": entry[systime_index],
                 "low": entry[low_index],
                 "high": entry[high_index],
                 "now": entry[now_index],
-            })
+            }
+            if new_normalized_data not in normalized_data[ticker]:
+                normalized_data[ticker].append(new_normalized_data)
             logger.info("Added normalized entry for ticker %s", ticker)
             break  # берём первую актуальную строку CETS и выходим
-                
+
     return normalized_data
 
 
